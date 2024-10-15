31d2a4c5aa2e58056736ee77181fbcdec9e5cc67
Hugo Duran <hugo.duran0625@gmail.com>
2024-10-15 14:57:15 +0200

    d

--- ConsoleApp1/Source/Arena.cs
+++ ConsoleApp1/Source/Arena.cs
@@ -5,41 +5,6 @@ using Boll;
 namespace Arena {
     public class Arena
 {
-    public void createArena() {
-        int ballX = 15;
-        int ballY = 57;
-        int[,] ball = new int[ballX, ballY];
-        int rows = 20; // Number of rows
-        int columns = 80; // Number of columns
-        char[,] grid = new char[rows, columns];
-
-                for (int row = 0; row < rows; row++)
-        {
-            for (int col = 0; col < columns; col++)
-            {
-                grid[row, col] = '+'; // Assign a "+" to each coordinate
-            }
-        }
-
-        for (int row = 0; row < rows; row++)
-        {
-            for (int col = 0; col < columns; col++)
-            if (row == 0 || row == 19) {
-                Console.Write("_");
-            }
-            else if (row == ballX && col == ballY) {
-                Console.Write("O");
-            }
-            else
-            {
-                if (row > 0 && (col == 0 || col == 79)) {
-                Console.Write("|");
-            }
-            else
-                Console.Write(" ");
-            }
-            Console.WriteLine(); // Move to the next line after completing a row
-        }
-    }
+   
 }
 }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77495f7355c966751ecb392598d15263322778bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4693b22e3cfcd57263fb7da05ada4e38722ec906")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

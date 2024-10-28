f60d0a6486435ec0871e369262553c8b2bb9f043
Petterkalle <92868522+Petterkalle@users.noreply.github.com>
2024-10-28 14:53:00 +0100

    Meny
    LAgt till en fungerande meny

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main()
     {
-        Arena arenan = new Arena(20, 80);
+        /* Arena arenan = new Arena(20, 80);
         Ball ball1 = new Ball(40, 10);
         IAbility playerAbi = new Screw(ball1);
         IAbility playerAbi2 = new Smash(ball1);
@@ -13,6 +13,33 @@ class Program
         humanRacketBuilder racket2 = new humanRacketBuilder(78, 10, false, playerAbi2);
 
 
-        Game game = new Game(ball1, racket1, racket2, arenan);
+        Game game = new Game(ball1, racket1, racket2, arenan); */
+        
+        Arena arenan = new Arena(20, 80);
+        Ball ball1 = new Ball(40, 10);
+        IAbility playerAbi = new Screw(ball1);
+        IAbility playerAbi2 = new Smash(ball1);
+        
+        StartingMenu menu = new StartingMenu();
+        int selectedIndex = menu.MenuReturnIndex();
+
+        if (selectedIndex == 0) 
+        {
+            humanRacketBuilder racket1 = new humanRacketBuilder(1, 10, true, playerAbi);
+            humanRacketBuilder racket2 = new humanRacketBuilder(78, 10, false, playerAbi2);
+            Game game = new Game(ball1, racket1, racket2, arenan);
+        }
+        else if (selectedIndex == 1)
+        {
+            humanRacketBuilder racket1 = new humanRacketBuilder(1, 10, true, playerAbi);
+            computerRacketBuilder racket2 = new computerRacketBuilder(78, 10, false, ball1, playerAbi2);
+            Game game = new Game(ball1, racket1, racket2, arenan);
+        }
+        else if (selectedIndex == 2)
+        {
+            computerRacketBuilder racket1 = new computerRacketBuilder(1, 10, true, ball1, playerAbi);
+            computerRacketBuilder racket2 = new computerRacketBuilder(78, 10, false, ball1, playerAbi2);
+            Game game = new Game(ball1, racket1, racket2, arenan);
+        }
     }
 }
--- ConsoleApp1/Source/Menu.cs
+++ ConsoleApp1/Source/Menu.cs
@@ -1,26 +1,91 @@
 
-public interface IMenu
+public class IMenu
 {
-    void up();
-    void down();
-    void select();
+
+    protected static void DrawMenu(string[] menu, int index)
+    {
+        Console.Clear();
+        for (int i = 0; i < menu.Length; i++)
+        {
+            if (i == index) 
+            {
+                Console.WriteLine($"[{menu[i]}]");
+            }
+            else 
+            {   
+                Console.WriteLine($"{menu[i]}");
+            }
+        }
+    }
+    protected void Scroll(string[] menu, ref int index)
+    {
+        bool selecting = true;
+    
+        while(selecting)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.W:
+                    index = PreviousIndex(menu, index);
+                    break;
+                case ConsoleKey.UpArrow:
+                    index = PreviousIndex(menu, index);
+                    break;
+                case ConsoleKey.S:
+                    index = NextIndex(menu, index);
+                    break;
+                case ConsoleKey.DownArrow:
+                    index = NextIndex(menu, index);
+                    break;
+                case ConsoleKey.Enter:
+                    selecting = false;
+                    break;
+ 
+            }
+
+            DrawMenu(menu, index);
+
+        }
+    }
+    protected int NextIndex(string[] menu, int index)
+    {
+        index++;
+
+        if (index >= menu.Length)
+        {
+            index = 0;
+        }
+        return index;
+    }
+
+    protected int PreviousIndex(string[] menu, int index)
+    {
+        index--;
+        if (index < 0)
+        {
+            index = menu.Length - 1; 
+        }
+        return index;
+    }
 }
 
 public class StartingMenu : IMenu
 {
-    public void up()
+    public int MenuReturnIndex()
     {
+        Console.WriteLine("Welcome to Pong!");
 
-    }
+        string[] startingMenu = {"Player vs. Player", "Player vs. Computer", "Computer vs. Computer"};   
+        int index = 0;
 
-    public void down()
-    {
+        Console.WriteLine("What kind of game do you want to play?");
+        DrawMenu(startingMenu, index);
+        Scroll(startingMenu, ref index);
 
-    }
+        Console.Clear(); 
 
-    public void select()
-    {
-
+        return index;
     }
 
 
@@ -28,18 +93,5 @@ public class StartingMenu : IMenu
 
 public class EndMenu : IMenu
 {
-    public void up()
-    {
-
-    }
-
-    public void down()
-    {
-
-    }
-
-    public void select()
-    {
-
-    }
+    
 }

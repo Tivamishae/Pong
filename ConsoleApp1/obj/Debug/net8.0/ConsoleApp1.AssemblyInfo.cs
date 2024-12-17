e28f0cafc6b1cb7b982d95e94269be63adc0ee56
Hugo Duran <hugo.duran0625@gmail.com>
2024-12-17 14:45:55 +0100

    h

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dfad936a4ed105ef3305eb0128d60ae250f57ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c60920c37b612eb00cf973c17b22f3b87e402671")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
